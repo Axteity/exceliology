b1f8f752876beed6c705be8ce6381093ef916011
Axteity <nekit_miheev@bk.ru>
2023-07-26 15:41:59 +0500

    main menu output (first steps)

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from mainmenu import Ui_MainWindow
+
+# Application instance creation
+app = QApplication(sys.argv)
+
+# Main window instance creation
+window = QMainWindow()
+
+# UI widgets instances creation
+ui = Ui_MainWindow()
+ui.setupUi(window)
+
+# Displaying window
+window.show()
+
+# Starting event handler loop
+sys.exit(app.exec())
